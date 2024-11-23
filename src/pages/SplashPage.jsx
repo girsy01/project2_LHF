71d66196c49430faf4526f0f34bc095961eb1002
Hernâni Silva <hcsilvamatos@hotmail.com>
2024-11-23 14:06:45 +0000

    core routes setup, splashpage without styling, not found page

--- src/pages/SplashPage.jsx
+++ src/pages/SplashPage.jsx
@@ -1,6 +1,16 @@
+import logo from '../assets/logo.png'
 const SplashPage = () => {
   return (
-    <div>SplashPage</div>
+    <div>
+        <img src={logo} alt="" />
+        <label>Username:
+            <input type="text" />
+        </label>
+        <label>Password:
+            <input type="password" />
+        </label>
+        <button>Login</button>
+    </div>
   )
 }
 export default SplashPage
