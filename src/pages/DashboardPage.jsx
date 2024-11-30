99e90e965ae7f2ac1f422e9feef9c6603867a92c
Hernâni Silva <hcsilvamatos@hotmail.com>
2024-11-30 16:28:44 +0000

    moved all the cards inside the card-container

--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -22,9 +22,7 @@ const DashboardPage = () => {
 
   const { categoryFilter, setCategoryFilter } = useContext(FilterContext);
 
-  useEffect(() => {
-    axios.get("http://localhost:5005/user").then((response) => {
-      const data = response.data;
+  
 
       useEffect(() => {
         axios.get("http://localhost:5005/user").then((response) => {
@@ -36,11 +34,7 @@ const DashboardPage = () => {
         });
       }, [userId]);
 
-      const user = data.find((oneUser) => oneUser.id === userId);
 
-      setCurrentUser(user);
-    });
-  }, [currentUser]);
 
   return (
     <div className="dashboardPage">
@@ -63,38 +57,17 @@ const DashboardPage = () => {
             {currentUser.movies.map((movie, index) => (
               <ItemCard key={index} category="movie" item={movie} />
             ))}
-          </div>
-        )}
-
-        {/* Books */}
-        {currentUser.books && (
-          <div className="card-container">
-            {/* <h3>Books</h3> */}
             {currentUser.books.map((book, index) => (
               <ItemCard key={index} category="book" item={book} />
             ))}
-          </div>
-        )}
-
-        {/* Music */}
-        {currentUser.music && (
-          <div className="card-container">
-            {/* <h3>Music</h3> */}
             {currentUser.music.map((music, index) => (
               <ItemCard key={index} category="music" item={music} />
             ))}
-          </div>
-        )}
-
-        {/* Events */}
-        {currentUser.events && (
-          <div className="card-container">
-            {/* <h3>Events</h3> */}
             {currentUser.events.map((event, index) => (
               <ItemCard key={index} category="event" item={event} />
             ))}
           </div>
-        )}
+        )}      
 
         {/* <div className="card-container">
           {myItems
