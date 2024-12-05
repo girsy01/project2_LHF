14dfa9113a2877af2e904c1353dbef271c8df87c
Franziska Scheer <franzi-scheer@web.de>
2024-12-05 21:29:59 +0100

    profile image, not finished yet

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useRef, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../contexts/AuthContext";
 import axios from "axios";
 import { Link, useNavigate } from "react-router-dom";
@@ -10,6 +10,24 @@ const ProfilePage = () => {
   const navigate = useNavigate();
   const { loggedIn, setLoggedIn, setUserId } = useContext(AuthContext);
   const { setMessage, setShowDeleteMessage } = useContext(MessageContext);
+  const [profileImage, setProfileImage] = useState(null);
+  const [imageUrl, setImageUrl] = useState("");
+
+  useEffect(() => {
+    if (userId) {
+      axios
+        .get("http://localhost:5005/user")
+        .then((response) => {
+          const data = response.data;
+          const user = data.find((oneUser) => String(oneUser.id) === String(userId));
+          setCurrentUser(user || {}); // Fallback to an empty object if no user is found
+          setImageUrl(user.image_url);
+        })
+        .catch((error) => {
+          console.error("Error fetching user data:", error);
+        });
+    }
+  }, [userId]);
 
   function handleLogout() {
     setLoggedIn(false);
@@ -40,20 +58,30 @@ const ProfilePage = () => {
     }
   }
 
+  function handleAddImage(e) {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append("file", profileImage);
+    formData.append("upload_preset", "Exhibitly");
+    axios
+      .post("https://api.cloudinary.com/v1_1/dj6l8ij7d/image/upload", formData)
+      .then((res) => {
+        setImageUrl(res.data.url);
+        console.log("Image url:", res.data.url);
+      })
+      .catch((err) => console.log(err));
+  }
+
   useEffect(() => {
-    if (userId) {
-      axios
-        .get("http://localhost:5005/user")
-        .then((response) => {
-          const data = response.data;
-          const user = data.find((oneUser) => String(oneUser.id) === String(userId));
-          setCurrentUser(user || {}); // Fallback to an empty object if no user is found
-        })
-        .catch((error) => {
-          console.error("Error fetching user data:", error);
-        });
-    }
-  }, [userId]);
+    axios
+      .patch(`http://localhost:5005/user/${userId}`, { image_url: imageUrl })
+      .then(({ data }) => {
+        // console.log("Image added to user");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, [imageUrl]);
 
   return (
     <div className="wrapper profilePage">
@@ -65,6 +93,12 @@ const ProfilePage = () => {
       ) : (
         <h3>Currently not logged in</h3>
       )}
+      {imageUrl && <img src={imageUrl} className="profileImage" />}
+      <form onSubmit={handleAddImage}>
+        <p>Profile Image</p>
+        <input type="file" onChange={(e) => setProfileImage(e.target.files[0])} />
+        <button className="btn-dark">Upload</button>
+      </form>
       {loggedIn ? (
         <>
           <button className="btn-light" onClick={handleLogout}>
